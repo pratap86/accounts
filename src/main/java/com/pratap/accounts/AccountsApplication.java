efd7fee99aeaa45cc5d50b1ab724c18495a9cd00
pratap86 <narayanpratap86@gmail.com>
2022-09-06 10:32:59 +0530

    service read the changes from configurator

--- src/main/java/com/pratap/accounts/AccountsApplication.java
+++ src/main/java/com/pratap/accounts/AccountsApplication.java
@@ -1,8 +1,10 @@
 package com.pratap.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pratap/accounts/controller/AccountsController.java
+++ src/main/java/com/pratap/accounts/controller/AccountsController.java
@@ -1,7 +1,12 @@
 package com.pratap.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.pratap.accounts.config.AccountsServiceConfig;
 import com.pratap.accounts.model.Accounts;
 import com.pratap.accounts.model.Customer;
+import com.pratap.accounts.model.Properties;
 import com.pratap.accounts.repository.AccountsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,9 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    private AccountsServiceConfig accountsServiceConfig;
+
     @GetMapping("/myAccount")
     public ResponseEntity<Accounts> getAccountDetails(@RequestBody Customer customer) {
 
@@ -27,5 +35,14 @@ public class AccountsController {
         if (accounts != null)
             return new ResponseEntity<Accounts>(accounts, HttpStatus.FOUND);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/properties")
+    public String getPropertiesDetails() throws JsonProcessingException {
+
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsServiceConfig.getMsg(), accountsServiceConfig.getBuildVersion(),
+                accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
+        return objectWriter.writeValueAsString(properties);
     }
 }
